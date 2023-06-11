7c217bcc57e72b8e266b6e1c55682caed98b2bc4
Rohan Chari <rohan.rchari@gmail.com>
2023-06-11 15:05:11 -0400

    Added basic backend  functionality for player add

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+let inputList = [];
+let playerObjectList = [];
+
 app.get("/", function(req,res){
-    res.render("index");
+    res.render("index",{inputList:inputList});
 });
 
 app.listen(3000, function(){
     console.log("App listening on port 3000");
+})
+
+app.post("/", function(req,res){
+    inputList.push(req.body.playerSearch);
+    res.redirect("/")
 })
