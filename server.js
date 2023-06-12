9013282ef4077313b4c006d21f2638e3ad166827
Rohan Chari <rohan.rchari@gmail.com>
2023-06-12 09:45:34 -0400

    Add basic front end and back end for year input

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-let inputList = [];
+let playerList = [];
+let yearList = [];
 let playerObjectList = [];
 
 app.get("/", function(req,res){
-    res.render("index",{inputList:inputList});
+    res.render("index",{playerList:playerList,yearList:yearList});
 });
 
 app.listen(3000, function(){
@@ -20,6 +21,12 @@ app.listen(3000, function(){
 })
 
 app.post("/", function(req,res){
-    inputList.push(req.body.playerSearch);
+    playerList.push(req.body.playerSearch);
+    yearList.push(req.body.yearSearch)
     res.redirect("/")
+})
+app.get("/reset",function(req,res){
+    inputList = [];
+    yearList = [];
+    res.redirect("/");
 })
